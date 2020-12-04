2011c6bef955e14fd4812b20a2e9178688f6465d
Renato Santos <renato_jfs@hotmail.com>
2020-12-03 22:20:44 -0300

    Correções

---
+++ src/app.js
@@ -0,0 +1,39 @@
+// Exemplo de implementação - exercício de arrastar e soltar
+
+var exemplo_arrastar_soltar =
+{
+	"id": "grupo01",
+	"pares": [
+		{
+			"arrastar": {
+				"id": "id-01",
+				"src": "figura01.png",
+				"descricao_title": "Descrição da figura 1"
+			},
+			"soltar": {
+				"id": "id-02",
+				"src": "figura02.png",
+				"descricao_title": "Descrição da figura 2"
+			}
+		},
+		{
+			"arrastar": {
+				"id": "id-03",
+				"src": "figura03.png",
+				"descricao_title": "Descrição da figura 3"
+			},
+			"soltar": {
+				"id": "id-04",
+				"src": "figura04.png",
+				"descricao_title": "Descrição da figura 4"
+			}
+		},
+	],
+	"distracoes": [
+		{
+			"id": "id-05",
+			"src": "figura05.png",
+			"descricao_title": "Descrição da figura 5",
+		}
+	],
+}
